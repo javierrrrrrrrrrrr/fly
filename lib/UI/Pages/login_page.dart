e6e52375cd113b0e4cf902ef004b18490f1fec53
Javier <javierserrano98050602@gmail.com>
2022-09-20 11:08:08 -0400

    suiguiendo con login

--- lib/UI/Pages/login_page.dart
+++ lib/UI/Pages/login_page.dart
@@ -7,24 +7,37 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage("assets/fondoNubes.jpg"),
-          fit: BoxFit.fill,
-        )),
-        child: Padding(
-          padding: EdgeInsets.only(top: size.height * 0.031),
-          child: Container(
-            height: size.height * 0.5,
-            width: size.width,
-            decoration: const BoxDecoration(
-                // color: Colors.red,
-                image: DecorationImage(
-                    image: AssetImage("assets/loginIcono.png"),
-                    fit: BoxFit.fill)),
+      body: Stack(
+        children: [
+          Container(
+              height: size.height,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("assets/fondoNubes.jpg"),
+                fit: BoxFit.fill,
+              ))),
+          Padding(
+            padding: EdgeInsets.only(top: size.height * 0.03),
+            child: Container(
+              height: size.height * 0.8,
+              width: size.width,
+              decoration: const BoxDecoration(
+                  // color: Colors.red,
+                  color: Colors.red,
+                  image: DecorationImage(
+                      image: AssetImage("assets/loginIcono.png"),
+                      fit: BoxFit.fill)),
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              height: size.height * 0.1,
+              width: size.width * 0.1,
+              color: Colors.blue,
+            ),
+          ),
+        ],
       ),
     );
   }
